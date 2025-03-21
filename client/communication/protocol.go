d6a855b202fa0bf80dd28051ebbd527c156fb8dc
Alan Botoshansky <91224532+AlanBotoshansky@users.noreply.github.com>
2025-03-21 12:43:13 -0300

    ej5
    Nombre y Apellido vuelven a tener tamaño variable. El tamaño se indica con un byte en cada caso.

--- client/communication/protocol.go
+++ client/communication/protocol.go
@@ -11,12 +11,12 @@ const (
 	MessageTypeSize = 1
 	HeaderSize      = LenMessageSize + MessageTypeSize
 
-	IdAgenciaSize  = 4
-	NombreSize     = 32
-	ApellidoSize   = 32
-	DniSize        = 8
-	NacimientoSize = 10
-	NumeroSize     = 4
+	IdAgenciaSize   = 4
+	LenNombreSize   = 1
+	LenApellidoSize = 1
+	DniSize         = 8
+	NacimientoSize  = 10
+	NumeroSize      = 4
 )
 
 const (
@@ -57,8 +57,8 @@ func SerializeBet(bet BetMessage) []byte {
 
 	messageSize := 0
 	messageSize += IdAgenciaSize
-	messageSize += NombreSize
-	messageSize += ApellidoSize
+	messageSize += LenNombreSize + len(nombreBytes)
+	messageSize += LenApellidoSize + len(apellidoBytes)
 	messageSize += DniSize
 	messageSize += NacimientoSize
 	messageSize += NumeroSize
@@ -75,11 +75,15 @@ func SerializeBet(bet BetMessage) []byte {
 	binary.BigEndian.PutUint32(buffer[pos:pos+IdAgenciaSize], bet.IdAgencia)
 	pos += IdAgenciaSize
 
-	copy(buffer[pos:pos+NombreSize], nombreBytes)
-	pos += NombreSize
+	buffer[pos] = uint8(len(nombreBytes))
+	pos += LenNombreSize
+	copy(buffer[pos:pos+len(nombreBytes)], nombreBytes)
+	pos += len(nombreBytes)
 
-	copy(buffer[pos:pos+ApellidoSize], apellidoBytes)
-	pos += ApellidoSize
+	buffer[pos] = uint8(len(apellidoBytes))
+	pos += LenApellidoSize
+	copy(buffer[pos:pos+len(apellidoBytes)], apellidoBytes)
+	pos += len(apellidoBytes)
 
 	copy(buffer[pos:pos+DniSize], dniBytes)
 	pos += DniSize
